87aaf5a05b9fa2a108ecbeaad5b80ebdfc93cbda
Isaac Olawale <bodolawale@gmail.com>
2021-05-06 21:55:49 +0100

    added error handler

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,13 @@ const PORT = process.env.PORT || 2323;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
 });
+
+function errorHandler(err: any, req: any, res: any, next: any) {
+	res.status(500).send({
+		success: false,
+		message: "An error occurred while processing this request",
+		data: err.toString(),
+	});
+}
+
+app.use(errorHandler);
