497c542a2f0739bed71d560f982d3cfc774a2f4d
Isaac Olawale <bodolawale@gmail.com>
2021-05-06 22:16:49 +0100

    completed get endpiont

--- src/item/item.repository.ts
+++ src/item/item.repository.ts
@@ -1,7 +1,8 @@
 import Item, { IItem } from "./item.model";
+import { Op } from "sequelize";
 export default class ItemRepository {
 	static async getItem(item: string): Promise<IItem[]> {
-		return Item.findAll({ where: { item } });
+		return Item.findAll({ where: { item, expiry: { [Op.gt]: Date.now() } } });
 	}
 
 	static async addItem(itemDTO: IItem): Promise<IItem> {
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -8,7 +8,14 @@ export default class ItemService {
 	}
 
 	public async getItem(item: string) {
-		return this.itemRepository.getItem(item);
+		const items = await this.itemRepository.getItem(item);
+		let quantity = 0;
+		let validTill = Infinity;
+		items.forEach((item) => {
+			quantity += item.quantity;
+			validTill = item.expiry < validTill ? item.expiry : validTill;
+		});
+		return { quantity, validTill };
 	}
 	public async addItem(item: string, quantity: number, expiry: number) {
 		return this.itemRepository.addItem({ item, quantity, expiry });
