12f463ef13db18de480c7d7cb0c30b686758c83b
Isaac Olawale <bodolawale@gmail.com>
2021-05-07 12:09:28 +0100

    initial tests setup

---
+++ src/tests/item/item.service.test.ts
@@ -0,0 +1,8 @@
+import assert from "assert";
+describe("ItemService", () => {
+	describe("addItem", () => {
+		it("should successfully add an item", () => {
+			assert.strictEqual(true, true);
+		});
+	});
+});
