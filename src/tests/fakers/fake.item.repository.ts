c34a434d01bc276856a3caecb063c9c1a049a3cb
Isaac Olawale <bodolawale@gmail.com>
2021-05-07 12:43:15 +0100

    added fake item repository

---
+++ src/tests/fakers/fake.item.repository.ts
@@ -0,0 +1,42 @@
+import { IItem } from "./../../item/item.model";
+
+export default class FakeItemRepository {
+	public id: number;
+	public items: IItem[];
+
+	constructor() {
+		this.id = 1;
+		this.items = [];
+	}
+
+	async addItem(itemDTO: IItem) {
+		this.id = this.id++;
+		itemDTO.id = this.id;
+		this.items.push(itemDTO);
+	}
+
+	async getItem(itemName: string) {
+		const items = this.items.filter(
+			({ item, expiry }) => item === itemName && expiry > Date.now()
+		);
+		return items.sort((a, b) => a.expiry - b.expiry);
+	}
+
+	async updateItemQuantity(id: number, quantity: number) {
+		const index = this.getItemById(id);
+		this.items[index].quantity = quantity;
+	}
+
+	async deleteItem(id: number) {
+		const index = this.getItemById(id);
+		this.items.splice(index, 1);
+	}
+
+	async deleteExpiredItems() {
+		this.items = this.items.filter(({ expiry }) => expiry > Date.now());
+	}
+
+	private getItemById(itemId: number) {
+		return this.items.findIndex(({ id }) => id === itemId);
+	}
+}
